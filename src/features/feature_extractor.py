221440cce55469cc76b0ac2a960570c65b531a78
Roi zini <roi.zini12@gmail.com>
2022-05-09 20:34:11 +0300

    features extractor

---
+++ src/features/feature_extractor.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy import signal
+
+def RTF(data):
+    '''Relative Transfer Function'''
+    '''Recived data is in shape->(mics num, samples)'''
+    stft_sig = []
+    for i in range(data.shape[0]):
+        sig = data[i,:] #  i is mic number
+        sig = sig / max(abs(sig)) # normalized data
+        stft_sig.appand(signal.stft(sig))
+    
+    for i in range(data.shape[0]):
+        ref_mic_stft = stft_sig[0]
+        stft_sig[i] = stft_sig[i] / ref_mic_stft
+    
+    return np.real(stft_sig), np.imag(stft_sig)
+
+
+
