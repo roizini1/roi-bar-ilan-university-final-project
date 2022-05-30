4995215a74260fb7acab4816ce71639cd5780ed9
Roi zini <roi.zini12@gmail.com>
2022-05-30 20:52:19 +0300

    30/05/2022

--- src/data/upload_pickle.py
+++ src/data/upload_pickle.py
@@ -2,29 +2,73 @@
 from yaml import safe_load
 # import random
 import torch
+import torchvision
 import pickle
+import sys
+sys.path.append('/home/dsi/ziniroi/roi-aviad/src/features')
 from feature_extractor import RTF
+import numpy as np
 
 # configs' args upload:
 stream = open("/home/dsi/ziniroi/roi-aviad/config_try.yaml", 'r')
 dictionary = safe_load(stream)
 data_init = dictionary.get("Data")
 
-#scenario_list = os.listdir(data_init.get("results_path").get("train"))
-#def Data_Loader(train_list,):
-#    shuffled_list = random.shuffle(scenario_list)
-batch_size = 32
+batch_size = 2
 
-def pickle_loader(input):
+def mix_loader(input):
+    mix_index = 0
     item = pickle.load(open(input, 'rb'))
-    return item.values
+    return item[mix_index]
 
-train_data= torch.torchvision.datasets.DatasetFolder(root=data_init.get("results_path").get("train"), loader=pickle_loader, extensions='.pickle', transform=RTF)
+def is_valid_file(input):
+    item = pickle.load(open(input, 'rb'))
+    return not(np.isnan(item).any())
+
+def collate_batch(batch):
+  
+  data = []
+  
+  for i in batch:
+    processed = torch.tensor(loader, dtype=torch.int64)
+    data.append(processed)
+  
+  list = pad_sequence(data, batch_first=True, padding_value=0)
+  
+  return list.to(device)
+
+# root = data_init.get("results_path").get("train")
+train_data = torchvision.datasets.DatasetFolder(root='/home/dsi/ziniroi/roi-aviad/data/raw', loader=mix_loader, extensions='.p', transform=RTF)#, is_valid_file=is_valid_file
 train_loader = torch.utils.data.DataLoader(
-        dataset=train_data,
-        batch_size=batch_size,
-        shuffle=True)
+        dataset = train_data,
+        batch_size = batch_size,
+        collate_fn=torch.nn.utils.rnn.pad_sequence,
+        shuffle = True)
 
+
+
+
+
+
+indxs = 0
+max_num = 0
+examples_num = 0
+for i in train_loader:
+    all_dim = i[0][0].shape # indexs 0 is real indexs 1 is imag
+    # print(all_dim)
+    sh = all_dim[3]
+    if indxs == 0:
+        min_num = sh
+        indxs = 1
+
+    min_num = np.copy(np.min([sh,min_num]))
+    max_num = np.copy(np.max([sh,max_num]))
+    examples_num += 1
+print(min_num,max_num)
+print(examples_num)
+# min = 199 ,max = 1614
+# min = 170 max = 2047
+# 25500
 
 
 
--- src/features/feature_extractor.py
+++ src/features/feature_extractor.py
@@ -5,13 +5,20 @@ def RTF(data):
     '''Relative Transfer Function'''
     '''Recived data is in shape->(mics num, samples)'''
     stft_sig = []
-    for i in range(data.shape[0]):
+    mics_num = data.shape[0] # mics num
+
+    for i in range(mics_num):
         sig = data[i,:] #  i is mic number
-        sig = sig / max(abs(sig)) # normalized data
-        stft_sig.appand(signal.stft(sig))
+        sig = np.array(sig / max(abs(sig)),dtype='f') # normalized data
+        stft_calc = signal.stft(sig,nperseg=512, window="hamming", noverlap=512 * 0.75)[2]  ##might be better with other params
+        #print(np.shape(stft_calc))
+        stft_sig.append(np.array(stft_calc))
     
-    for i in range(data.shape[0]):
-        ref_mic_stft = stft_sig[0]
-        stft_sig[i] = stft_sig[i] / ref_mic_stft
+    
+    ref_mic_stft =  np.array(stft_sig[0])
+    for i in range(mics_num):
+        stft_sig[i] =  np.array(stft_sig[i]) /  ref_mic_stft
+    # print(ref_mic_stft.shape)
+    
     
     return np.real(stft_sig), np.imag(stft_sig)
