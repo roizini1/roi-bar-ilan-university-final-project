1d04a0d1e6ec2c9803fd5bfdcbb74b54a0ba6930
Roi zini <roi.zini12@gmail.com>
2022-06-07 10:27:19 +0300

    07/06/2022

--- src/data/upload_pickle.py
+++ src/data/upload_pickle.py
@@ -1,13 +1,14 @@
-# import os
 from yaml import safe_load
-# import random
-import torch
 import torchvision
+import numpy as np
+import torch
 import pickle
+
 import sys
 sys.path.append('/home/dsi/ziniroi/roi-aviad/src/features')
+
 from feature_extractor import RTF
-import numpy as np
+
 
 # configs' args upload:
 stream = open("/home/dsi/ziniroi/roi-aviad/config_try.yaml", 'r')
@@ -16,59 +17,45 @@ data_init = dictionary.get("Data")
 
 batch_size = 2
 
-def mix_loader(input):
+def loader(input):
     mix_index = 0
+    target_index = 1
+    
+    fs = 16000
+    seconds = 3
+    samples = seconds * fs
+
     item = pickle.load(open(input, 'rb'))
-    return item[mix_index]
+    mix = item[mix_index]
+    length = mix[:].shape[1]
+    
+    min_index = min(length,samples)
+    mix_slice = mix[:][0:min_index]
+    mix_out = torch.zeros([mix[:].shape[0],samples])
+    mix_out[:,0:min_index] = torch.tensor(mix_slice[:,0:min_index]) 
+    
+    target = item[target_index]
+    target_slice = torch.tensor(target[:][0:min_index])
+    target_out = torch.zeros([target_slice.shape[0],samples])
+    target_out[:,0:min_index] = target_slice[:,0,0:min_index]
+
+    return mix_out ,target_out
 
 def is_valid_file(input):
     item = pickle.load(open(input, 'rb'))
     return not(np.isnan(item).any())
 
-def collate_batch(batch):
-  
-  data = []
-  
-  for i in batch:
-    processed = torch.tensor(loader, dtype=torch.int64)
-    data.append(processed)
-  
-  list = pad_sequence(data, batch_first=True, padding_value=0)
-  
-  return list.to(device)
 
-# root = data_init.get("results_path").get("train")
-train_data = torchvision.datasets.DatasetFolder(root='/home/dsi/ziniroi/roi-aviad/data/raw', loader=mix_loader, extensions='.p', transform=RTF)#, is_valid_file=is_valid_file
+
+root = data_init.get("results_path").get("train")
+train_data = torchvision.datasets.DatasetFolder(root=root, loader=loader, extensions='.p', transform=RTF)  #, is_valid_file=is_valid_file
 train_loader = torch.utils.data.DataLoader(
         dataset = train_data,
         batch_size = batch_size,
-        collate_fn=torch.nn.utils.rnn.pad_sequence,
         shuffle = True)
 
 
-
-
-
-
-indxs = 0
-max_num = 0
-examples_num = 0
-for i in train_loader:
-    all_dim = i[0][0].shape # indexs 0 is real indexs 1 is imag
-    # print(all_dim)
-    sh = all_dim[3]
-    if indxs == 0:
-        min_num = sh
-        indxs = 1
-
-    min_num = np.copy(np.min([sh,min_num]))
-    max_num = np.copy(np.max([sh,max_num]))
-    examples_num += 1
-print(min_num,max_num)
-print(examples_num)
-# min = 199 ,max = 1614
-# min = 170 max = 2047
-# 25500
-
-
-
+for batch in train_loader:
+    x,y = batch
+    all_dim = x.shape
+    print(all_dim)
--- src/features/feature_extractor.py
+++ src/features/feature_extractor.py
@@ -1,24 +1,49 @@
 import numpy as np
+import torch
 from scipy import signal
 
 def RTF(data):
     '''Relative Transfer Function'''
     '''Recived data is in shape->(mics num, samples)'''
-    stft_sig = []
-    mics_num = data.shape[0] # mics num
-
+    stft_mix = []
+    # stft_target = []
+    mix , target = data
+    # mix = data
+    # print(mix.shape,target.shape)
+    mics_num = mix.shape[0] # mics num
+    # num_of_speakers = target.shape[0]
+    
+    # mix handle:
+    i = 0
     for i in range(mics_num):
-        sig = data[i,:] #  i is mic number
-        sig = np.array(sig / max(abs(sig)),dtype='f') # normalized data
+        sig = mix[i,:] #  i is mic number
+        
+        sig = sig / max(abs(sig)) # normalized data
         stft_calc = signal.stft(sig,nperseg=512, window="hamming", noverlap=512 * 0.75)[2]  ##might be better with other params
-        #print(np.shape(stft_calc))
-        stft_sig.append(np.array(stft_calc))
+        stft_mix.append(stft_calc)
     
+    # print('mix shape: '+str(stft_mix[0].shape))
+    stft_shape = stft_mix[0].shape
+
+    #j = 0
+    #for j in range(num_of_speakers):
+    #    sig = target[j,0,:] #  i is mic number
+    #    sig = sig / max(abs(sig)) # normalized data
+    #    stft_calc = signal.stft(sig,nperseg=512, window="hamming", noverlap=512 * 0.75)[2]  ##might be better with other params
+    #    stft_target.append(stft_calc)
     
-    ref_mic_stft =  np.array(stft_sig[0])
+
+    rtf = torch.zeros([mics_num,2,stft_shape[0],stft_shape[1]],dtype=torch.cfloat)
+    ref_mic_stft = stft_mix[0]
+
+    i = 0
     for i in range(mics_num):
-        stft_sig[i] =  np.array(stft_sig[i]) /  ref_mic_stft
-    # print(ref_mic_stft.shape)
+        temp = torch.tensor(stft_mix[i] /  ref_mic_stft,dtype=torch.cfloat)
+        # print(temp.shape)
+        rtf[i,0,:] = temp.real
+        rtf[i,1,:] = temp.imag
     
+    #real_imag_mix =                                        #torch.cat((torch.tensor(rtf,dtype=torch.cfloat).real, torch.tensor(rtf,dtype=torch.cfloat).imag,2))
     
-    return np.real(stft_sig), np.imag(stft_sig)
+    # real_imag_target = torch.cat((torch.tensor(stft_target,dtype=torch.cfloat).real(), torch.tensor(stft_target,dtype=torch.cfloat).imag()),2)
+    return  rtf , target         #real_imag_mix , real_imag_target
